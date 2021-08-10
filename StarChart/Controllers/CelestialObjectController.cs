87b7c63fecf9068d7ba4d165efcd30265af21084
Huhogithub <88107338+Huhogithub@users.noreply.github.com>
2021-08-10 14:14:11 -0400

    add actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,6 +17,46 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+        [HttpGet("{id:int}",Name ="GetById")]
+        public IActionResult GetById(int id)
+        {
+            var co = _context.CelestialObjects.Find(id);
+            if (co != null)
+            {
+                return Ok(co);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var co = _context.CelestialObjects.Where(x=>x.Name==name).ToList();
+            if (co != null)
+            {
+                foreach (var c in co)
+                {
+                    c.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == c.Id).ToList();
+                }
+                return Ok(co);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetAll(string name)
+        {
+            var co = _context.CelestialObjects.ToList();
+            foreach (var c in co)
+            {
+                c.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == c.Id).ToList();
+            }
+            return Ok(co);
         }
     }
 }
